13f7c77fa9e826b62e2a851c31befb281c8a3079
Matt Delves <matt@reformedsoftware.com>
2014-06-07 15:03:34 -0700

    add in projects for QuickCore and QuickExpectations

---
+++ QuickCore/QuickCore/QuickCore.h
@@ -0,0 +1,19 @@
+//
+//  QuickCore.h
+//  QuickCore
+//
+//  Created by Matthew Delves on 7/06/2014.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QuickCore.
+FOUNDATION_EXPORT double QuickCoreVersionNumber;
+
+//! Project version string for QuickCore.
+FOUNDATION_EXPORT const unsigned char QuickCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickCore/PublicHeader.h>
+
+
---
+++ QuickExpectations/QuickExpectations/QuickExpectations.h
@@ -0,0 +1,19 @@
+//
+//  QuickExpectations.h
+//  QuickExpectations
+//
+//  Created by Matthew Delves on 7/06/2014.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QuickExpectations.
+FOUNDATION_EXPORT double QuickExpectationsVersionNumber;
+
+//! Project version string for QuickExpectations.
+FOUNDATION_EXPORT const unsigned char QuickExpectationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickExpectations/PublicHeader.h>
+
+
